04789bbea4623460397a439c1e06f46b87e3b720
Yitzhak Rubin <yitzhakrubin16@gmail.com>
2026-09-06 11:48:42 +0300

    Initial React setup with routing

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Routes, Route } from "react-router"
+import HomePage from "./pages/HomePage"
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+    </Routes>
+      
+  )
+}
+
+export default App
