1306430d0322049634fd75634ad738accebcba91
Yitzhak Rubin <yitzhakrubin16@gmail.com>
2026-09-06 15:29:18 +0300

    Load and display products from API

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,49 @@
+import { useEffect, useState } from "react"
+import type { Product } from "../types/product"
 
 function HomePage() {
+
+  const [products, setProducts] = useState<Product[]>([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState("")
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+    .then((res) => {
+      if(!res.ok){
+        throw new Error("Failed to load products")
+      }
+
+      return res.json()
+    })
+    .then((data) => {
+      setProducts(data)
+      setLoading(false)
+    })
+    .catch(() => {
+      setError("Failed to load products")
+      setLoading(false)
+    })
+  }, [])
+
+  if(loading){
+    return <p>Loading...</p>
+  }
+  if(error){
+    return <p>{error}</p>
+  }
+
   return (
-    <div>Home Page</div>
+    <div>
+      <h1>Products</h1>
+
+      {products.map((product) => (
+        <div key={product.id}>
+          <h2>{product.title}</h2>
+          <p>${product.price}</p>
+        </div>
+      ))}
+    </div>
   )
 }
 
