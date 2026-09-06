dcf9a12227cb974ecbb8032d9ffc9faefd142a7f
Yitzhak Rubin <yitzhakrubin16@gmail.com>
2026-09-06 12:26:24 +0300

    Add header navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router"
+
 
 function Header() {
   return (
     <header>
         <h2>Mini shop</h2>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/favorites">Favorites</Link>
+        </nav>
     </header>
   )
 }
