2c2166ed8bfec60af0d7c1837a0bf4baec4bc78c
Yitzhak Rubin <yitzhakrubin16@gmail.com>
2026-09-06 16:49:30 +0300

    Add product details page

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,11 +1,49 @@
+import { useEffect, useState } from "react"
 import { useParams } from "react-router"
+import type { Product } from "../types/product"
 
 function ProductPage() {
   const { id } = useParams()
+
+  const [product, setProduct] = useState<Product | null>(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState("")
+  
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/products/${id}`)
+    .then((res) => {
+      if(!res.ok){
+        throw new Error ("Failed to load product")
+      }
+      return res.json()
+    })
+    .then((data) => {
+      setProduct(data)
+      setLoading(false)
+    })
+    .catch(() => {
+      setError("Failed to load product")
+      setLoading(false)
+    })
+  }, [id])
+
+  if(loading){
+    return <p>Loading...</p>
+  }
+  if(error){
+    return <p>{error}</p>
+  }
+
+  if (!product) {
+    return <p>Product not found</p>
+  }
   return (
     <div>
-      <h1>Product Page</h1>
-      <p>Product id: {id}</p>
+      <img src={product.image} alt={product.title} />
+      <h1>{product.title}</h1>
+      <p>{product.description}</p>
+      <p>{product.price}</p>
+      <p>{product.category}</p>
     </div>
   )
 }
