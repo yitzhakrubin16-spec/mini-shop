d39b4ccf31150fb82c45d08d3a4b4f06dd0bfe83
Yitzhak Rubin <yitzhakrubin16@gmail.com>
2026-09-07 10:28:35 +0300

    Add favorites toggle to product cards

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,11 +1,16 @@
 import type { Product } from '../types/product'
 import { Link } from 'react-router'
+import { useFavoritesStore } from '../store/favoritesStore'
 
 type ProductCardProps = {
     product: Product
 }
 
 function ProductCard({ product }: ProductCardProps) {
+  const { addFavorite, removeFavorite, isFavorite } = useFavoritesStore()
+  
+
+  const favorite = isFavorite(product.id)
   return (
     <div>
       <Link to={`/products/${product.id}`}>
@@ -17,6 +22,16 @@ function ProductCard({ product }: ProductCardProps) {
         <h2>{product.title}</h2>
         <p>${product.price}</p>
       </Link>
+      <button
+      onClick={() =>{
+        if (favorite){
+          removeFavorite(product.id)
+        } else {
+          addFavorite(product)
+        }
+      }}>
+        {favorite ? "♥" : "♡"}
+      </button>
     </div>
   )
 }
