af1031bcbf506f053472ec35836d5afea0bb5425
Yitzhak Rubin <yitzhakrubin16@gmail.com>
2026-09-07 11:49:44 +0300

    Add favorites page

--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -1,8 +1,22 @@
-
+import { useFavoritesStore } from "../store/favoritesStore"
+import ProductCard from "../components/ProductCard"
 
 function FavoritesPage() {
+  const {favorites} = useFavoritesStore()
   return (
-    <div>Favorites Page</div>
+    <div>
+      <h1>Favorites Page</h1>
+
+      {favorites.length === 0 ? (
+        <p>No favorites yet</p>
+      ) : (
+        favorites.map((product) => (
+        <ProductCard
+          key={product.id}
+          product={product}
+        />
+        )))}
+    </div>
   )
 }
 
