16957a9fa88d9bf47cce4e739f4384951ad7081d
Yitzhak Rubin <yitzhakrubin16@gmail.com>
2026-09-07 11:51:27 +0300

    Add favorites count to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router"
-
+import { useFavoritesStore } from "../store/favoritesStore"
 
 function Header() {
+  const { favorites } = useFavoritesStore()
+
   return (
     <header>
         <h2>Mini shop</h2>
         <nav>
           <Link to="/">Home</Link>
-          <Link to="/favorites">Favorites</Link>
+          <Link to="/favorites">Favorites ({favorites.length})</Link>
         </nav>
     </header>
   )
